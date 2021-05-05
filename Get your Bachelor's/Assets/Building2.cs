fe0ff5471531e79e850a49308fb91343400349ce
Severin Hanke <65894195+HankeKun@users.noreply.github.com>
2021-05-05 16:26:47 +0200

    Added Building 1

---
+++ Get your Bachelor's/Assets/Building2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building2 : MonoBehaviour
+{
+    private bool playerInRange;
+    public GameObject keyEnter;
+    public LevelLoader levelLoader;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            //levelLoader.whereToGo = 2;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            keyEnter.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            keyEnter.SetActive(false);
+        }
+    }
+}
