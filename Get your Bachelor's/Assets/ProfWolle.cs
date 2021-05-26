34bc0b7de831812e15838d4972dc4a4c1269ad61
Severin Hanke <65894195+HankeKun@users.noreply.github.com>
2021-05-26 17:27:51 +0200

    Added Prof Kurz

---
+++ Get your Bachelor's/Assets/ProfWolle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfWolle : MonoBehaviour
+{
+    //public GameObject dialogBox;
+    private bool playerInRange;
+    //public PlayerMovement playerMovement;
+    public GameObject keyEnter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            keyEnter.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            keyEnter.SetActive(false);
+        }
+    }
+}
