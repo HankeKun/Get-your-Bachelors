e227e2dc1a5bdaa8286045d30cdc6d83f7555ca3
Severin Hanke <65894195+HankeKun@users.noreply.github.com>
2021-08-25 15:48:12 +0200

    Changed the options Menu

--- Get your Bachelor's/Assets/SettingsMenu.cs
+++ Get your Bachelor's/Assets/SettingsMenu.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public void SetVolume(float volume)
-    {
-        audioMixer.SetFloat("volume", volume);
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i=0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void setResolution(int index) {
+        Resolution resolution = resolutions[index];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void setQuality(int index) {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullscreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
     }
 }
